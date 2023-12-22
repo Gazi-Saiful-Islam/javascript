bcf60fd6a42100399894a19886775538d0866354
Gazi-Saiful-Islam <gazisaifulislam894@gmail.com>
2023-12-23 01:48:31 +0300

    counting string length using charAt and swith

---
+++ index.js
@@ -0,0 +1,69 @@
+// counting length using charAt()
+
+/*
+
+var myName = 'saiful islam'
+var count = 0;
+while (true) {
+  if(myName.charAt(count)===''){
+    break;
+  }else{
+    count++;
+  }
+}
+console.log(count)
+
+*/
+
+//  switch
+
+/*
+
+
+var num = 2
+
+switch (num) {
+  case 3:
+   console.log('it is three') 
+    break;
+   
+  case 4:
+    console.log('it is four')
+  default:
+    console.log('nothing')
+    break;
+}
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
