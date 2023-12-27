fb953c2df3c98804e6b1fff7f49f2be92bd57f13
Gazi-Saiful-Islam <gazisaifulislam894@gmail.com>
2023-12-28 02:35:35 +0300

    string comparision,literal,notation . Array reverse emplements manually

--- index.js
+++ index.js
@@ -89,6 +89,126 @@ console.log(result)
 */
 
 
+//   loop will be stop when the random number will matched
+
+
+/*
+
+let isRuning= true
+
+while(isRuning){
+  let rand = Math.floor(Math.random()*10 + 1)
+  
+  if(rand===8){
+    console.log('we are win ' + rand)
+isRuning=false
+  }else{
+    console.log('you have got '+rand)
+  }
+}
+
+*/
+
+
+
+//string literal and string constructor
+
+
+/*
+
+let str = 'saiful'
+console.log(str)
+
+let str2 = String('saiful islam')
+console.log(str2)
+
+*/
+
+
+//escape notation
+
+/*
+
+
+let name ='my name is \'Gazi Saiful Islam\'.it is my full name.'
+
+console.log(name)
+
+
+*/
+
+
+//string comparision  (last side alphabets value more than first side alphabet value.)
+
+
+/*
+
+
+let first = 'acd'
+
+let second ='abc'
+
+if(first>second){
+  console.log('true')
+}else{
+  console.log('false')
+}
+
+*/
+
+//  array reverse emplement
+
+
+/*
+
+let arr =[34,45,67,43,54]
+
+for(let i = 0; i<arr.length/2; i++){
+  let temp=arr[i]
+ arr[i]=arr[arr.length-1-i]
+ arr[arr.length-1-i]=temp
+ 
+}
+
+console.log(arr)
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
